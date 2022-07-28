c4d55705c81abaa53b75e4b6294d5e6d87b188d1
marfurta <anamariamarfurt@gmail.com>
2022-07-28 02:06:57 +0000

    guardo app

--- src/app.py
+++ src/app.py
@@ -1,4 +1,45 @@
 from utils import db_connect
-engine = db_connect()
-
 # your code here
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt  
+#!pip install seaborn
+import seaborn as sns
+import seaborn as sb
+#!pip install plotly
+import plotly.graph_objects as go
+#!pip install folium
+#!pip install statsmodels
+import folium
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from folium.plugins import MarkerCluster
+from folium import plugins
+from folium.plugins import FastMarkerCluster
+from folium.plugins import HeatMap
+import plotly.express as px
+import pickle
+import statsmodels.api as sm
+
+#load data
+url = 'https://raw.githubusercontent.com/4GeeksAcademy/linear-regression-project-tutorial/main/medical_insurance_cost.csv'
+df_raw=pd.read_csv(url)
+
+
+pd.get_dummies(df_raw,drop_first=True)
+
+# drop duplicates
+df_raw=df_raw.drop_duplicates().reset_index(drop= True)
+
+
+#loaded_model = pickle.load(open(filename, 'rb'))
+filename = 'finalized_model.sav'
+pickle.load(modelo5, open("../models/filename, 'rb'"))
+
+#Predict using the model whith new data
+print('Predicted ] : \n', loaded_model.predict(X_test[40,1,22,1,1,1,0,0]))
+
